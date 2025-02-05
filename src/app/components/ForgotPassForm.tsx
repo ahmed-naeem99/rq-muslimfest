e7bbcff84a96ad7347cb00364bea163a91fb01ab
HBehery <hamzahbehery@gmail.com>
2025-02-05 01:14:11 -0500

    frontend setup with API call

--- src/app/components/ForgotPassForm.tsx
+++ src/app/components/ForgotPassForm.tsx
@@ -6,7 +6,31 @@ import Image from "next/image";
 import realityQuestLogo from "../../../public/rqlogo.svg";
 
 export default function ForgotPassForm() {
-  const [email, setEmail] = useState(" ");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const validateEmail = (email: string) => {
+    // Regular expression for username validation
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async () => {
+    if (!validateEmail(email)) {
+      setEmailError("Invalid email format.");
+    } else {
+      setEmailError("");
+      //api call to send reset link
+      const response = await fetch("/api/forgot-password", {
+        method: "POST",
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+    }
+  };
 
   return (
     <div className="grid place-items-center">
@@ -14,7 +38,7 @@ export default function ForgotPassForm() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col items-center">
           <Image priority src={realityQuestLogo} alt="Logo" width={300} />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-sky-950 dark:text-white">
-            Forgot Password
+            Reset Password
           </h2>
         </div>
 
@@ -37,16 +61,21 @@ export default function ForgotPassForm() {
                   required
                   className="block w-full px-3 rounded-md border-0 bg-black/5 dark:bg-white/5 py-1.5 dark:text-white shadow-sm ring-1 ring-inset ring-black/5 dark:ring-white/10 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
                 />
+                {emailError && (
+                  <p className="text-red-500 pt-3 w-full max-w-xs">
+                    {emailError}
+                  </p>
+                )}
               </div>
             </div>
 
             <div className="pt-6">
               <button
-                onClick={() => null}
+                onClick={handleSubmit}
                 disabled={!email}
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-sky-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Send Forgot Password Email
+                Send Reset Link
               </button>
             </div>
           </div>
