84b2764ba7ffefa4fff0d8d6310b312af30504ad
beheryh1 <hamzah.behery@mail.utoronto.ca>
2025-03-28 04:08:35 -0400

    stuck navbar to top and changed bg colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import NavBar from "./components/navbar/NavBar";
 import { NextAuthProvider } from "./components/NextAuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MAC Reality Quest",
@@ -16,9 +19,13 @@ export default async function RootLayout({
   return (
     <NextAuthProvider>
       <html lang="en" className="h-full dark">
-        <body className="dark:bg-dark h-full w-full flex flex-col transition-colors">
-          <NavBar />
-          <div className="flex-1 overflow-auto"> {children} </div>
+        <body
+          className={`dark:bg-gray-950 h-full w-full flex flex-col transition-colors p-0 ${inter.className}`}
+        >
+          <div className="sticky top-0 z-50">
+            <NavBar />
+          </div>
+          <div className="relative overflow-auto">{children}</div>
         </body>
       </html>
     </NextAuthProvider>
