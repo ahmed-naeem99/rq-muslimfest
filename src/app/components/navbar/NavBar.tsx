4dc32906065616c45943513c56d956f9eef16cf8
HBehery <hamzahbehery@gmail.com>
2025-04-04 04:31:41 -0400

    updated dropdown colours + click off toggle

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,14 +1,58 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 import Image from "next/image";
-import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
 import Logout from "./Logout";
 import realityQuestLogo from "../../../../public/rqlogo.svg";
 import { RxAvatar } from "react-icons/rx";
 
-const NavBar = async () => {
-  const session = await getServerSession();
+const NavBar = () => {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent | TouchEvent) => {
+      if (!detailsRef.current || !isOpen) return;
+
+      const target = event.target as Node;
+      if (!detailsRef.current.contains(target)) {
+        detailsRef.current.open = false;
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener("touchstart", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("touchstart", handleOutsideClick);
+    };
+  }, [isOpen]);
+
+  const handleToggle = (e: React.SyntheticEvent<HTMLDetailsElement, Event>) => {
+    const details = e.currentTarget;
+
+    setTimeout(() => {
+      if (details) {
+        const isCurrentlyOpen = details.open;
+        setIsOpen(isCurrentlyOpen);
+      }
+    }, 0);
+  };
+
+  const toggleDropdown = () => {
+    if (detailsRef.current) {
+      const newState = !detailsRef.current.open;
+      detailsRef.current.open = newState;
+      setIsOpen(newState);
+    }
+  };
+
   return (
     <div className="backdrop-blur-lg bg-white dark:bg-black/10  navbar  dark:text-white dark:focus:text-white transition-colors sm:px-6 px-2">
       <div className="navbar-start">
@@ -21,12 +65,19 @@ const NavBar = async () => {
       <div className="navbar-center">
         <ul className="menu menu-horizontal px-1 items-center justify-center flex">
           <li>
-            <details>
-              <summary>Missions</summary>
-              <ul className="p-2  bg-sky-400 dark:bg-sky-600">
+            <details ref={detailsRef} onToggle={handleToggle}>
+              <summary
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleDropdown();
+                }}
+              >
+                Missions
+              </summary>
+              <ul className="p-2  bg-sky-400 dark:bg-neutral-600">
                 <li>
                   <Link
-                    className="dark:focus:text-white text-white"
+                    className="dark:focus:text-white focus:text-white text-white"
                     href="/mission/1"
                   >
                     M1
@@ -34,7 +85,7 @@ const NavBar = async () => {
                 </li>
                 <li>
                   <Link
-                    className="dark:focus:text-white text-white"
+                    className="dark:focus:text-white focus:text-white text-white"
                     href="/mission/2"
                   >
                     M2
@@ -42,7 +93,7 @@ const NavBar = async () => {
                 </li>
                 <li>
                   <Link
-                    className="dark:focus:text-white text-white"
+                    className="dark:focus:text-white focus:text-white text-white"
                     href="/mission/3"
                   >
                     M3
@@ -53,7 +104,7 @@ const NavBar = async () => {
           </li>
           <li>
             <Link
-              className="dark:focus:text-white dark:text-white"
+              className="dark:focus:text-white focus:text-black dark:text-white"
               href="/leaderboard"
             >
               Leaderboard
@@ -75,13 +126,15 @@ const NavBar = async () => {
           </div>
           <ul
             tabIndex={0}
-            className=" mt-24 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52  bg-sky-400 dark:bg-sky-600"
+            className=" mt-24 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52  bg-sky-400 dark:bg-neutral-600"
           >
             {!session && (
               <li>
-                <Link className="text-white  justify-between" href="/login">
+                <Link
+                  className="dark:focus:text-white focus:text-white text-white  justify-between "
+                  href="/login"
+                >
                   Login
-                  {/* <span className="badge">New</span> */}
                 </Link>
               </li>
             )}
